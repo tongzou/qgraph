b00081cf84775410dc87cd46ace55d1cab84f61e
tongzou <tong@cal.berkeley.edu>
2016-07-06 12:45:07 -0700

    added tests

--- test/manhattan-spec.js
+++ test/manhattan-spec.js
@@ -11,7 +11,12 @@ global.navigator = {};
 describe('ManhattanLink', function() {
 	it('should route', function() {
 		var qg = require('../dist/qgraph').default;
-		var link = qg.Link.getLink([{point: new qg.Point(150, 40), direction: qg.Point.W}, {point: new qg.Point(0, -40), direction: qg.Point.W}], {type: 'manhattan', MIN_BUFFER: 15});
+		var input = [{point: new qg.Point(150, 40), direction: qg.Point.W}, {point: new qg.Point(0, -40), direction: qg.Point.W}];
+		var link = qg.Link.getLink(input, {type: 'manhattan', MIN_BUFFER: 15});
 		expect(link.points).to.eql([new qg.Point(150, 40), new qg.Point(-15, 40), new qg.Point(-15, -14), new qg.Point(0, -40)]);
+
+		/*input = [];
+		link = qg.Link.getLink(input, {type: 'manhattan', MIN_BUFFER: 15});
+		expect();*/
 	});
 });
